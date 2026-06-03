3d9217a32cf2502ba7284d6cca516b4b89a2a7d3
Dmitry <Dololob92@gmail.com>
2026-06-03 11:41:17 +0300

    fix: server

--- server/src/main.ts
+++ server/src/main.ts
@@ -11,6 +11,7 @@ async function bootstrap() {
       'http://localhost:5173',
       'http://localhost:4173',
       'http://localhost:5174',
+      'https://ecommerce-application-lumw.onrender.com',
     ],
     credentials: true,
   });
